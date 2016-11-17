9303ee0f4426a28a45108378a63c6780594119c5
Fernando Daciuk <f.daciuk@gmail.com>
2016-11-17 21:06:41 -0200

    Update webpack.prod

--- storybook/webpack.prod.config.js
+++ storybook/webpack.prod.config.js
@@ -4,29 +4,25 @@ const path = require('path')
 const webpack = require('webpack')
 const validate = require('webpack-validator')
 
-const HtmlPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
 module.exports = validate({
-  entry: path.join(__dirname, 'src', 'index'),
+  entry: path.join(__dirname, '..', 'src', 'index'),
+
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
 
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name]-[hash].js'
+    path: path.join(__dirname, '..', 'dist'),
+    filename: '[name]-[hash].js',
+    libraryTarget: 'umd'
   },
 
   plugins: [
-    new ExtractTextPlugin('[name]-[hash].css'),
-
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': '"production"'
       }
-    }),
-
-    new HtmlPlugin({
-      title: 'GitHub app',
-      template: path.join(__dirname, 'src', 'html', 'template.html')
     }),
 
     new webpack.optimize.UglifyJsPlugin({
@@ -50,18 +46,13 @@ module.exports = validate({
       exclude: /node_modules/,
       include: /src/,
       loader: 'babel'
-    }, {
-      test: /\.css$/,
-      exclude: /node_modules/,
-      include: /src/,
-      loader: ExtractTextPlugin.extract('style', 'css')
     }]
   },
 
   resolve: {
     alias: {
-      src: path.join(__dirname, 'src'),
-      components: path.join(__dirname, 'src', 'components')
+      src: path.join(__dirname, '..', 'src'),
+      components: path.join(__dirname, '..', 'src', 'components')
     }
   }
 })
