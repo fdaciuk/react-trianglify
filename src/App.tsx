87acfaee923be127df39e71054ac3ec207f10d27
Fernando Daciuk <f.daciuk@gmail.com>
2024-05-02 20:16:56 -0300

    fix: types for trianglify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import trianglify from "trianglify"
 import { Trianglify } from "react-trianglify"
 import './App.css'
 
@@ -6,11 +5,7 @@ function App() {
   return (
     <>
       <h1>React Trianglify</h1>
-      <Trianglify
-        key={Math.random()}
-        colorFunction={trianglify.colorFunctions.shadows(0.5)}
-        palette={trianglify.utils.colorbrewer}
-      />
+      <Trianglify />
     </>
   )
 }
