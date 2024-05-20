b465859e3c1bef1096b57557f4bab797f4a3a11a
Fernando Daciuk <f.daciuk@gmail.com>
2024-05-20 19:55:19 -0300

    fix: vite config to build .d.ts files

--- vite.config-lib.ts
+++ vite.config-lib.ts
@@ -2,16 +2,21 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tsConfigPaths()
+    tsConfigPaths(),
+    dts({
+      entryRoot: resolve(__dirname, 'src', 'lib'),
+      outDir: 'lib',
+    }),
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src', 'lib', 'react-trianglify.jsx'),
+      entry: resolve(__dirname, 'src', 'lib', 'react-trianglify.tsx'),
       name: 'ReactTrianglify',
       fileName: 'react-trianglify',
     },
