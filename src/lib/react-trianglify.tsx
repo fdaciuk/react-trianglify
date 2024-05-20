68ac8715b735d980a744a5c76601b9132df54e6b
Fernando Daciuk <f.daciuk@gmail.com>
2024-05-20 19:53:43 -0300

    fix: convert react-trianglify from jsx to tsx

---
+++ src/lib/react-trianglify.tsx
@@ -0,0 +1,39 @@
+import { useRef, useEffect } from 'react'
+import trianglify, { type Options } from 'trianglify'
+
+export type TrianglifyProps = Options & Partial<{
+  output: "canvas" | "svg"
+}>
+
+export function Trianglify({
+  output = 'canvas',
+  width = 600,
+  height = 400,
+  ...props
+}: TrianglifyProps) {
+  const refCanvas = useRef<HTMLCanvasElement | null>(null)
+  const refSVG = useRef<SVGSVGElement | null>(null)
+  useEffect(() => {
+    const pattern = trianglify({
+      width,
+      height,
+      ...props
+    })
+
+    if (output === "canvas" && refCanvas.current) {
+      pattern.toCanvas(refCanvas.current)
+    }
+
+    if (output === "svg" && refSVG.current) {
+      pattern.toSVG(refSVG.current)
+    }
+  }, [props, width, height, output])
+
+  if (output === "svg") {
+    return <svg ref={refSVG} width={width} height={height} />
+  }
+
+  return <canvas ref={refCanvas} width={width} height={height} />
+}
+
+export default Trianglify
